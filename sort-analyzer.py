ff794996a0d46763c02166a23789a89b6bda1b1f
a.wei4 <andyzihengwei@gmail.com>
2023-11-07 13:30:27 -0700

    happyhappyhappy

--- sort-analyzer.py
+++ sort-analyzer.py
@@ -2,6 +2,7 @@
 
 import time
 
+
 # RETURN DATA FROM FILE AS AN ARRAY OF INTERGERS
 def loadDataArray(fileName):
     temp = []
@@ -35,3 +36,65 @@ print(fewUniqueData[0:50])
 # bubbleSort(randomData)
 # endTime = time.time()
 # print(f"Bubble Sort Random Data: {endTime - startTime} seconds")
+
+def timing(function_to_call, array_to_sort):
+    startTime = time.time()
+    function_to_call(array_to_sort)
+    endTime = time.time()
+    timeTaken = endTime - startTime
+    return timeTaken
+
+
+def bubbleSort(anArray):
+    for n in range(len(anArray) - 1):
+        for i in range(len(anArray) - n - 1):
+            if anArray[i] > anArray[i + 1]:
+                anArray[i], anArray[i + 1] = anArray[i + 1], anArray[i]
+
+    return
+
+
+def selectionSort(anArray):
+    for slot in range(len(anArray) - 1):
+        min_position = slot
+        for i in range(min_position + 1, len(anArray)):
+            if anArray[i] < anArray[min_position]:
+                min_position = i
+        (anArray[slot], anArray[min_position]) = (anArray[min_position], anArray[slot])
+
+
+def insertionSort(anArray):
+    for i in range(1, len(anArray)):
+        item = anArray[i]
+        previous_position = i - 1
+        while previous_position >= 0 and item < anArray[previous_position]:
+            anArray[previous_position + 1] = anArray[previous_position]
+            previous_position -= 1
+        anArray[previous_position + 1] = item
+
+
+def average(function_to_call, amount_of_turns_to_run):
+    arrays_to_sort = [randomData, reversedData, nearlySortedData, fewUniqueData]
+    average_list = []
+
+    for item in arrays_to_sort:
+        for i in range(amount_of_turns_to_run):
+            startTime = time.time()
+            function_to_call(item)
+            endTime = time.time()
+            timeTaken = endTime - startTime
+
+            average_list.append(timeTaken)
+
+        average_calculate = 0
+        for item in average_list:
+            average_calculate += item
+
+        result = average_calculate / amount_of_turns_to_run
+
+        print(f"average time of sorting is {result}s")
+
+
+
+
+average(bubbleSort, 5)
